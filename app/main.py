6fcd9163bbd4c7aa40bdd40c5b019561de4c1b19
Joan266 <joanalemany26@gmail.com>
2025-07-27 15:19:40 +0200

    feat: -Added chat endpoint, with context of the report results so the user can ask questions related to fraud listing - Added data-extraction endpoint, so the iser dose a better work informing about the listing.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from app.core.limiter import limiter 
 from app.api.endpoints import router as api_router
 from app.core.config import settings
 from app.db import models 
@@ -8,12 +10,16 @@ from app.db.session import engine
 
 # This creates your database tables based on your models.
 models.Base.metadata.create_all(bind=engine)
+# Create a limiter that uses the client's IP address
+limiter = Limiter(key_func=get_remote_address)
 
 # Create the main FastAPI application instance
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 # A list of allowed origins for CORS (Cross-Origin Resource Sharing)
 # This is important for allowing your frontend application to communicate with the API
