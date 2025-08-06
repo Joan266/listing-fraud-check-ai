ea9474c313d55c2807b71679f69e9d1f46c79686
Joan266 <joanalemany26@gmail.com>
2025-08-07 00:48:23 +0200

    trying to deploy the workers with new script

--- alembic/env.py
+++ alembic/env.py
@@ -4,11 +4,15 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.db.models import Base
 from alembic import context
-
+import os
+from app.core.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+# Lee la DATABASE_URL desde la variable de entorno,
+# que es leída por tu objeto 'settings'.
+# Esto sobreescribirá la configuración de alembic.ini
+config.set_main_option('sqlalchemy.url', settings.DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
