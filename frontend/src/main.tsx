40b46761010e37d85285d51c0034a160bf2a204f
Joan Alemany Chulilla <jac@anialegal.com>
2026-08-06 14:00:01 +0200

    feat(analytics): add Vercel Analytics to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,16 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter> 
+        <BrowserRouter>
           <App />
+          <Analytics />
         </BrowserRouter>
       </Provider>
   </React.StrictMode>,
